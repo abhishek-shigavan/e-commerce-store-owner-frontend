aa5d28bb24b0b33125daeca40e48915d340b67c8
BL-Abhishek-Shigavan <abhishek.shigavan@bridgelabz.com>
2025-03-02 01:07:44 +0530

    MODIFIED: header created

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,18 @@
 import "./ProductList.scss"
+import logo from "../../assets/monklogo.svg"
 
 function ProductList () {
     return (
-        <>
-            <h1>ProductList</h1>
-        </>
+        <main className="product-list-main-cnt">
+            <header className="product-list-header-cnt">
+                <img src={logo} alt="monk-logo" />
+                <span>Monk Upsell & Cross-sell</span>
+            </header>
+            <section className="product-list-content-cnt">
+                <span>Add Products</span>
+                <button>Add Product</button>
+            </section>
+        </main>
     )
 }
 
