26a454c5d5ef25387b8dae8739e9126d266815d8
BL-Abhishek-Shigavan <abhishek.shigavan@bridgelabz.com>
2025-03-03 00:16:42 +0530

    MODIFIED: added title & search bar for product picker

--- src/components/ProductPicker/ProductPicker.jsx
+++ src/components/ProductPicker/ProductPicker.jsx
@@ -1,10 +1,20 @@
 import "./ProductPicker.scss"
+import closeIcon from "../../assets/close_icon.svg"
+import searchIcon from "../../assets/search_icon.svg"
 
 function ProductPicker () {
     return (
-        <>
-            <h1>ProductPicker</h1>
-        </>
+        <div className="product-picker-main-cnt">
+            <div className="product-picker-title-cnt">
+                <span>Select Products</span>
+                <img src={closeIcon} alt="Close icon" />
+            </div>
+            <div className="product-picker-search-cnt">
+                <img src={searchIcon} alt="Search icon" />
+                <input type="text" placeholder="Search product"/>
+            </div>
+            <div className="product-search-divider-cnt"></div>
+        </div>
     )
 }
 
