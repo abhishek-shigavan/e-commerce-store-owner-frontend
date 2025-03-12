5b7a426115616205fdcd015331cf6de3a4ba158a
BL-Abhishek-Shigavan <abhishek.shigavan@bridgelabz.com>
2025-03-12 17:50:34 +0530

    CREATE: exporting as icons from single file

---
+++ src/assets/IconnsConfig.js
@@ -0,0 +1,19 @@
+import bullet_icon from "./bullet_icon.svg"
+import close_grey_icon from "./close_grey_icon.svg"
+import close_icon from "./close_icon.svg"
+import down_arrow_icon from "./down_arrow.svg"
+import monk_logo_icon from "./monklogo.svg"
+import pen_icon from "./pen_icon.svg"
+import search_icon from "./search_icon.svg"
+import up_arrow_icon from "./up_arrow.svg"
+
+export {
+    bullet_icon as bulletIcon,
+    close_grey_icon as closeGreyIcon,
+    close_icon as closeIcon,
+    down_arrow_icon as downArrowIcon,
+    monk_logo_icon as logo,
+    pen_icon as penIcon,
+    search_icon as searchIcon,
+    up_arrow_icon as upArrowIcon
+}
